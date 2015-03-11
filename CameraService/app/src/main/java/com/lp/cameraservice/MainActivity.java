1dd290d7aa80765c5ffd9c7fa25b3c37ca634553
Manar Anabtawi <mo.anabtawi@hotmail.com>
2015-03-11 16:12:03 +0200

    Camera

---
+++ CameraService/app/src/main/java/com/lp/cameraservice/MainActivity.java
@@ -0,0 +1,122 @@
+package com.lp.cameraservice;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.PixelFormat;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.IOException;
+
+public class MainActivity extends Activity implements SurfaceHolder.Callback{
+
+    private Camera camera;
+    private SurfaceView surfaceView;
+    private SurfaceHolder surfaceHolder;
+    private boolean previewing = false;
+    private LayoutInflater controlInflater = null;
+    private Button takePhoto ;
+    private ImageView viewPhoto;
+
+    public void onCreate(Bundle savedInstanceState) {
+
+         super.onCreate(savedInstanceState);
+         setContentView(R.layout.main);
+         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+         getWindow().setFormat(PixelFormat.UNKNOWN);
+         surfaceView = (SurfaceView)findViewById(R.id.camerapreview);
+         surfaceHolder = surfaceView.getHolder();
+         surfaceHolder.addCallback(this);
+         surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+         controlInflater = LayoutInflater.from(getBaseContext());
+         View viewControl = controlInflater.inflate(R.layout.control, null);
+         LayoutParams layoutParamsControl = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
+         this.addContentView(viewControl, layoutParamsControl);
+         takePhoto  = (Button)  viewControl.findViewById(R.id.take_photo);
+         viewPhoto = (ImageView) viewControl.findViewById(R.id.image_view);
+
+         takePhoto.setOnClickListener(new View.OnClickListener() {
+
+             public void onClick(View v) {
+                 camera.takePicture(myShutterCallback,myPictureCallback_RAW, myPictureCallback_JPG);
+             }
+         });
+
+    }
+
+    Camera.ShutterCallback myShutterCallback = new Camera.ShutterCallback(){
+        public void onShutter() {
+
+
+        }};
+
+    Camera.PictureCallback myPictureCallback_RAW = new Camera.PictureCallback(){
+
+
+        public void onPictureTaken(byte[] arg0, Camera arg1) {
+            try {
+                camera.setPreviewDisplay(surfaceHolder);
+                camera.startPreview();
+                previewing = true;
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            }
+
+        }};
+
+    Camera.PictureCallback myPictureCallback_JPG = new Camera.PictureCallback(){
+
+        public void onPictureTaken(byte[] arg0, Camera arg1) {
+
+            Bitmap bitmapPicture= BitmapFactory.decodeByteArray(arg0, 0, arg0.length);
+            viewPhoto.setImageBitmap(bitmapPicture);
+
+        }};
+
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+        if(previewing){
+          camera.stopPreview();
+          previewing = false;
+        }
+
+        if (camera != null){
+             try {
+              camera.setPreviewDisplay(surfaceHolder);
+              camera.startPreview();
+              previewing = true;
+             } catch (IOException e) {
+
+              e.printStackTrace();
+             }
+        }
+    }
+
+
+    public void surfaceCreated(SurfaceHolder holder) {
+
+      camera = Camera.open();
+    }
+
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+        camera.stopPreview();
+        camera.release();
+        camera = null;
+        previewing = false;
+    }
+}
